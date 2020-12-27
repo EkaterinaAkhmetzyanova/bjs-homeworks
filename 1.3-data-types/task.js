c3fdb55865d1a0632abeaf4b2d98d3fc83e22986
Ekaterina Akhmetzyanova <a.ekaterina323@gmail.com>
2020-12-27 22:56:26 +0300

    homework 1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,33 @@
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    let morgageDetails = {
+        percent,
+        contribution,
+        amount
+    }
+
+    for (let prop in morgageDetails) {
+        if (Number.isNaN(parseInt(morgageDetails[prop]))) {
+            return `Параметр ${prop} содержит неправильное значение ${morgageDetails[prop]}`;
+        }
+    }
+
+    let S = amount - contribution;
+    let currentDate = new Date();
+    let n = (date.getFullYear() - currentDate.getFullYear()) * 12 - (currentDate.getMonth() - date.getMonth());
+    let P = percent / 100 / 12;
+    let paymentPerMonth = S * (P + P / (Math.pow(1 + P, n) - 1));
+    let totalAmount = (paymentPerMonth * n).toFixed(2);
+    console.log(totalAmount);
+    return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    //let username;
+    if (name == null || name == '' || name == undefined) {
+        name = 'Аноним';
+    }
+
+    return `Привет, мир! Меня зовут ${name}.`;
 }
