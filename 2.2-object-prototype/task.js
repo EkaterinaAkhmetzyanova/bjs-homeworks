9719be26900ec98589e9bc064b18ef573dbaa587
Ekaterina Akhmetzyanova <a.ekaterina323@gmail.com>
2020-12-30 01:20:13 +0300

    homework 2.2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,39 @@
-//String.prototype.isPalindrome - для задачи №1
+"use strict"
+//Задача 1
+function isPalindrome(string) {
+    this.string = string.toLowerCase().replace(/\s+/g, '').split('');
+    let originalString = this.string.join('');
+    let reversedString = this.string.reverse().join('');
+    if (originalString !== reversedString) {
+        return false;
+    } else {
+        return true;
+    }
+}
 
+String.prototype.isPalindrome = function() {
+    return this.string;
+}
+
+//Задача 2
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let marksSum = 0;
+    if (marks.length === 0) {
+        return 0;
+    }
+    for (let i = 0; i < marks.length; i++) {
+        marksSum += marks[i];
+    }
+    let average = marksSum / marks.length;
+    let roundedAverage = Math.round(average);
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = Date.now();
+    let birthdayDate = new Date(birthday);
+    birthday = +birthdayDate;
+    let diff = now - birthday;
+    let age = diff / (24 * 60 * 60 * 1000 * 365.25);
+    return (age >= 18);
 }
